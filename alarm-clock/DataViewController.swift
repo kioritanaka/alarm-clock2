e08b494ea121759a086f8b251d694e39bcc95ea9
Kiori Tanaka <kt191@duke.edu>
2019-02-16 14:32:15 -0500

    Initial Commit

---
+++ alarm-clock/DataViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DataViewController.swift
+//  alarm-clock
+//
+//  Created by Kiori Tanaka on 2/16/19.
+//  Copyright © 2019 pearlhacks. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
